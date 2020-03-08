28734aae369929566e3663001ad0fa384fa4e3b7
harupy <hkawamura0130@gmail.com>
2020-03-09 00:39:31 +0900

    Updates

--- src/main.py
+++ src/main.py
@@ -5,22 +5,33 @@
 # * This competition has 2 stages, so the main objective is to make a model that can predict the demand for the next 28 days
 
 # %% [code]
-import warnings
-
-warnings.filterwarnings("ignore")
-import pandas as pd
-import numpy as np
-
-pd.set_option("display.max_columns", 500)
-pd.set_option("display.max_rows", 500)
-import lightgbm as lgb
-from sklearn import preprocessing, metrics
 import gc
 import os
+import sys
+import warnings
 
-for dirname, _, filenames in os.walk("/kaggle/input"):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
+import pandas as pd
+import numpy as np
+import lightgbm as lgb
+from sklearn import preprocessing, metrics
+
+warnings.filterwarnings("ignore")
+pd.set_option("display.max_columns", 500)
+pd.set_option("display.max_rows", 500)
+
+
+# %% [code]
+def on_kaggle():
+    return "KAGGLE_KERNEL_RUN_TYPE" in os.environ
+
+
+# %% [code]
+if on_kaggle():
+    os.system("git clone https://github.com/harupy/m5-forecasting-accuracy")
+    os.system("pip install mlflow_extend")
+    sys.path.append("m5-forecasting-accuracy/src")
+else:
+    sys.path.append("src")
 
 # %% [code]
 def reduce_mem_usage(df, verbose=True):
@@ -65,134 +76,50 @@ def reduce_mem_usage(df, verbose=True):
 
 # function to read the data and merge it (ignoring some columns, this is a very fst model)
 
-
+# %% [code]
 def read_data():
+    input_dir = "/kaggle/input" if on_kaggle() else "input"
+    input_dir = f"{input_dir}/m5-forecasting-accuracy"
+
     print("Reading files...")
-    calendar = pd.read_csv("/kaggle/input/m5-forecasting-accuracy/calendar.csv")
-    calendar = reduce_mem_usage(calendar)
-    print(
-        "Calendar has {} rows and {} columns".format(
-            calendar.shape[0], calendar.shape[1]
-        )
+
+    calendar = pd.read_csv(f"{input_dir}/calendar.csv").pipe(reduce_mem_usage)
+    sell_prices = pd.read_csv(f"{input_dir}/sell_prices.csv").pipe(reduce_mem_usage)
+    sales_train_val = pd.read_csv(f"{input_dir}/sales_train_validation.csv").pipe(
+        reduce_mem_usage
     )
-    sell_prices = pd.read_csv("/kaggle/input/m5-forecasting-accuracy/sell_prices.csv")
-    sell_prices = reduce_mem_usage(sell_prices)
-    print(
-        "Sell prices has {} rows and {} columns".format(
-            sell_prices.shape[0], sell_prices.shape[1]
-        )
+    submission = pd.read_csv(f"{input_dir}/sample_submission.csv").pipe(
+        reduce_mem_usage
     )
-    sales_train_validation = pd.read_csv(
-        "/kaggle/input/m5-forecasting-accuracy/sales_train_validation.csv"
-    )
-    print(
-        "Sales train validation has {} rows and {} columns".format(
-            sales_train_validation.shape[0], sales_train_validation.shape[1]
-        )
-    )
-    submission = pd.read_csv(
-        "/kaggle/input/m5-forecasting-accuracy/sample_submission.csv"
-    )
-    return calendar, sell_prices, sales_train_validation, submission
+
+    print("Calendar shape:", calendar.shape)
+    print("Sell prices shape:", sell_prices.shape)
+    print("Sales train shape:", sales_train_val.shape)
+    print("Submission shape:", submission.shape)
+
+    return calendar, sell_prices, sales_train_val, submission
 
 
 def melt_and_merge(
-    calendar,
-    sell_prices,
-    sales_train_validation,
-    submission,
-    nrows=55000000,
-    merge=False,
+    calendar, sell_prices, sales_train_val, submission, nrows=55_000_000, merge=False,
 ):
-
     # melt sales data, get it ready for training
-    sales_train_validation = pd.melt(
-        sales_train_validation,
-        id_vars=["id", "item_id", "dept_id", "cat_id", "store_id", "state_id"],
-        var_name="day",
-        value_name="demand",
+    id_columns = ["id", "item_id", "dept_id", "cat_id", "store_id", "state_id"]
+    sales_train_val = pd.melt(
+        sales_train_val, id_vars=id_columns, var_name="day", value_name="demand",
     )
-    print(
-        "Melted sales train validation has {} rows and {} columns".format(
-            sales_train_validation.shape[0], sales_train_validation.shape[1]
-        )
-    )
-    sales_train_validation = reduce_mem_usage(sales_train_validation)
+    sales_train_val = reduce_mem_usage(sales_train_val)
 
-    # seperate test dataframes
-    test1_rows = [row for row in submission["id"] if "validation" in row]
-    test2_rows = [row for row in submission["id"] if "evaluation" in row]
-    test1 = submission[submission["id"].isin(test1_rows)]
-    test2 = submission[submission["id"].isin(test2_rows)]
+    # separate test dataframes
+    test1 = submission[submission["id"].str.contains("validation")]
+    test2 = submission[submission["id"].str.contains("evaluation")]
 
     # change column names
-    test1.columns = [
-        "id",
-        "d_1914",
-        "d_1915",
-        "d_1916",
-        "d_1917",
-        "d_1918",
-        "d_1919",
-        "d_1920",
-        "d_1921",
-        "d_1922",
-        "d_1923",
-        "d_1924",
-        "d_1925",
-        "d_1926",
-        "d_1927",
-        "d_1928",
-        "d_1929",
-        "d_1930",
-        "d_1931",
-        "d_1932",
-        "d_1933",
-        "d_1934",
-        "d_1935",
-        "d_1936",
-        "d_1937",
-        "d_1938",
-        "d_1939",
-        "d_1940",
-        "d_1941",
-    ]
-    test2.columns = [
-        "id",
-        "d_1942",
-        "d_1943",
-        "d_1944",
-        "d_1945",
-        "d_1946",
-        "d_1947",
-        "d_1948",
-        "d_1949",
-        "d_1950",
-        "d_1951",
-        "d_1952",
-        "d_1953",
-        "d_1954",
-        "d_1955",
-        "d_1956",
-        "d_1957",
-        "d_1958",
-        "d_1959",
-        "d_1960",
-        "d_1961",
-        "d_1962",
-        "d_1963",
-        "d_1964",
-        "d_1965",
-        "d_1966",
-        "d_1967",
-        "d_1968",
-        "d_1969",
-    ]
+    test1.columns = ["id"] + [f"d_{x}".format(x) for x in range(1914, 1914 + 28)]
+    test2.columns = ["id"] + [f"d_{x}".format(x) for x in range(1942, 1942 + 28)]
 
     # get product table
-    product = sales_train_validation[
-        ["id", "item_id", "dept_id", "cat_id", "store_id", "state_id"]
-    ].drop_duplicates()
+    product = sales_train_val[id_columns].drop_duplicates()
 
     # merge with product table
     test2["id"] = test2["id"].str.replace("_evaluation", "_validation")
@@ -200,66 +127,43 @@ def melt_and_merge(
     test2 = test2.merge(product, how="left", on="id")
     test2["id"] = test2["id"].str.replace("_validation", "_evaluation")
 
-    #
-    test1 = pd.melt(
-        test1,
-        id_vars=["id", "item_id", "dept_id", "cat_id", "store_id", "state_id"],
-        var_name="day",
-        value_name="demand",
-    )
-    test2 = pd.melt(
-        test2,
-        id_vars=["id", "item_id", "dept_id", "cat_id", "store_id", "state_id"],
-        var_name="day",
-        value_name="demand",
-    )
+    test1 = pd.melt(test1, id_vars=id_columns, var_name="day", value_name="demand",)
+    test2 = pd.melt(test2, id_vars=id_columns, var_name="day", value_name="demand",)
 
-    sales_train_validation["part"] = "train"
+    sales_train_val["part"] = "train"
     test1["part"] = "test1"
     test2["part"] = "test2"
 
-    data = pd.concat([sales_train_validation, test1, test2], axis=0)
+    data = pd.concat([sales_train_val, test1, test2], axis=0)
 
-    del sales_train_validation, test1, test2
+    del sales_train_val, test1, test2
 
-    # get only a sample for fst training
+    # get only a sample for fast training
     data = data.loc[nrows:]
 
     # drop some calendar features
-    calendar.drop(["weekday", "wday", "month", "year"], inplace=True, axis=1)
+    calendar = calendar.drop(["weekday", "wday", "month", "year"], axis=1)
 
     # delete test2 for now
     data = data[data["part"] != "test2"]
 
     if merge:
-        # notebook crash with the entire dataset (maybee use tensorflow, dask, pyspark xD)
+        # notebook crashes with the entire dataset
         data = pd.merge(data, calendar, how="left", left_on=["day"], right_on=["d"])
-        data.drop(["d", "day"], inplace=True, axis=1)
+        data = data.drop(["d", "day"], axis=1)
         # get the sell price data (this feature should be very important)
         data = data.merge(
             sell_prices, on=["store_id", "item_id", "wm_yr_wk"], how="left"
         )
-        print(
-            "Our final dataset to train has {} rows and {} columns".format(
-                data.shape[0], data.shape[1]
-            )
-        )
-    else:
-        pass
-
     gc.collect()
 
     return data
 
 
-calendar, sell_prices, sales_train_validation, submission = read_data()
+# %% [code]
+calendar, sell_prices, sales_train_val, submission = read_data()
 data = melt_and_merge(
-    calendar,
-    sell_prices,
-    sales_train_validation,
-    submission,
-    nrows=27500000,
-    merge=True,
+    calendar, sell_prices, sales_train_val, submission, nrows=27_500_000, merge=True
 )
 
 # %% [markdown]
@@ -267,12 +171,11 @@ data = melt_and_merge(
 
 # %% [code]
 def transform(data):
-
     nan_features = ["event_name_1", "event_type_1", "event_name_2", "event_type_2"]
     for feature in nan_features:
-        data[feature].fillna("unknown", inplace=True)
+        data[feature] = data[feature].fillna("MISSING")
 
-    cat = [
+    cat_cols = [
         "item_id",
         "dept_id",
         "cat_id",
@@ -283,37 +186,30 @@ def transform(data):
         "event_name_2",
         "event_type_2",
     ]
-    for feature in cat:
+    for col in cat_cols:
         encoder = preprocessing.LabelEncoder()
-        data[feature] = encoder.fit_transform(data[feature])
+        data[col] = encoder.fit_transform(data[col])
 
     return data
 
 
-def simple_fe(data):
-
+def feature_engineering(data):
     # rolling demand features
-    data["lag_t28"] = data.groupby(["id"])["demand"].transform(lambda x: x.shift(28))
-    data["lag_t29"] = data.groupby(["id"])["demand"].transform(lambda x: x.shift(29))
-    data["lag_t30"] = data.groupby(["id"])["demand"].transform(lambda x: x.shift(30))
-    data["rolling_mean_t7"] = data.groupby(["id"])["demand"].transform(
-        lambda x: x.shift(28).rolling(7).mean()
-    )
-    data["rolling_std_t7"] = data.groupby(["id"])["demand"].transform(
-        lambda x: x.shift(28).rolling(7).std()
-    )
-    data["rolling_mean_t30"] = data.groupby(["id"])["demand"].transform(
-        lambda x: x.shift(28).rolling(30).mean()
-    )
-    data["rolling_mean_t90"] = data.groupby(["id"])["demand"].transform(
-        lambda x: x.shift(28).rolling(90).mean()
-    )
-    data["rolling_mean_t180"] = data.groupby(["id"])["demand"].transform(
-        lambda x: x.shift(28).rolling(180).mean()
-    )
-    data["rolling_std_t30"] = data.groupby(["id"])["demand"].transform(
-        lambda x: x.shift(28).rolling(30).std()
-    )
+    for shift in [28, 29, 30]:
+        data[f"lag_t{shift}"] = data.groupby(["id"])["demand"].transform(
+            lambda x: x.shift(shift)
+        )
+
+    for size in [7, 30]:
+        data[f"rolling_std_t{size}"] = data.groupby(["id"])["demand"].transform(
+            lambda x: x.shift(28).rolling(size).std()
+        )
+
+    for size in [7, 30, 90, 180]:
+        data[f"rolling_mean_t{size}"] = data.groupby(["id"])["demand"].transform(
+            lambda x: x.shift(28).rolling(size).mean()
+        )
+
     data["rolling_skew_t30"] = data.groupby(["id"])["demand"].transform(
         lambda x: x.shift(28).rolling(30).skew()
     )
@@ -334,13 +230,14 @@ def simple_fe(data):
     data["price_change_t365"] = (
         data["rolling_price_max_t365"] - data["sell_price"]
     ) / (data["rolling_price_max_t365"])
+
     data["rolling_price_std_t7"] = data.groupby(["id"])["sell_price"].transform(
         lambda x: x.rolling(7).std()
     )
     data["rolling_price_std_t30"] = data.groupby(["id"])["sell_price"].transform(
         lambda x: x.rolling(30).std()
     )
-    data.drop(["rolling_price_max_t365", "lag_price_t1"], inplace=True, axis=1)
+    data = data.drop(["rolling_price_max_t365", "lag_price_t1"], axis=1)
 
     # time features
     data["date"] = pd.to_datetime(data["date"])
@@ -354,13 +251,50 @@ def simple_fe(data):
 
 
 def run_lgb(data):
+    features = [
+        "item_id",
+        "dept_id",
+        "cat_id",
+        "store_id",
+        "state_id",
+        "event_name_1",
+        "event_type_1",
+        "event_name_2",
+        "event_type_2",
+        "snap_CA",
+        "snap_TX",
+        "snap_WI",
+        "sell_price",
+        "lag_t28",
+        "lag_t29",
+        "lag_t30",
+        "rolling_mean_t7",
+        "rolling_std_t7",
+        "rolling_mean_t30",
+        "rolling_mean_t90",
+        "rolling_mean_t180",
+        "rolling_std_t30",
+        "price_change_t1",
+        "price_change_t365",
+        "rolling_price_std_t7",
+        "rolling_price_std_t30",
+        "rolling_skew_t30",
+        "rolling_kurt_t30",
+        "year",
+        "month",
+        "week",
+        "day",
+        "dayofweek",
+    ]
 
     # going to evaluate with the last 28 days
-    x_train = data[data["date"] <= "2016-03-27"]
-    y_train = x_train["demand"]
-    x_val = data[(data["date"] > "2016-03-27") & (data["date"] <= "2016-04-24")]
-    y_val = x_val["demand"]
-    test = data[(data["date"] > "2016-04-24")]
+    mask1 = data["date"] <= "2016-03-27"
+    mask2 = data["date"] <= "2016-04-24"
+    X_train = data[mask1]
+    y_train = X_train.pop("demand")
+    X_val = data[~mask1 & mask2]
+    y_val = X_val.pop("demand")
+    X_test = data[~mask2].drop("demand", axis=1)
     del data
     gc.collect()
 
@@ -370,94 +304,60 @@ def run_lgb(data):
         "metric": "rmse",
         "objective": "regression",
         "n_jobs": -1,
-        "seed": 236,
+        "seed": 42,
         "learning_rate": 0.1,
         "bagging_fraction": 0.75,
         "bagging_freq": 10,
         "colsample_bytree": 0.75,
     }
 
-    train_set = lgb.Dataset(x_train[features], y_train)
-    val_set = lgb.Dataset(x_val[features], y_val)
+    fit_params = {
+        "num_boost_round": 100_000,
+        "early_stopping_rounds": 50,
+        "verbose_eval": 100,
+    }
 
-    del x_train, y_train
+    train_set = lgb.Dataset(X_train[features], y_train)
+    val_set = lgb.Dataset(X_val[features], y_val)
 
-    model = lgb.train(
-        params,
-        train_set,
-        num_boost_round=2500,
-        early_stopping_rounds=50,
-        valid_sets=[train_set, val_set],
-        verbose_eval=100,
-    )
-    val_pred = model.predict(x_val[features])
-    val_score = np.sqrt(metrics.mean_squared_error(val_pred, y_val))
-    print(f"Our val rmse score is {val_score}")
-    y_pred = model.predict(test[features])
-    test["demand"] = y_pred
-    return test
+    del X_train, y_train
+
+    model = lgb.train(params, train_set, valid_sets=[train_set, val_set], **fit_params)
+
+    val_pred = model.predict(X_val[features])
+    val_rmse = np.sqrt(metrics.mean_squared_error(val_pred, y_val))
+    print(f"RMSE:", val_rmse)
+    y_pred = model.predict(X_test[features])
+    return model, X_test.assign(demand=y_pred)
 
 
-def predict(test, submission):
-    predictions = test[["id", "date", "demand"]]
-    predictions = pd.pivot(
-        predictions, index="id", columns="date", values="demand"
-    ).reset_index()
-    predictions.columns = ["id"] + ["F" + str(i + 1) for i in range(28)]
+def make_submission(test, submission):
+    preds = test[["id", "date", "demand"]]
+    preds = pd.pivot(preds, index="id", columns="date", values="demand").reset_index()
+    preds.columns = ["id"] + ["F" + str(i + 1) for i in range(28)]
 
-    evaluation_rows = [row for row in submission["id"] if "evaluation" in row]
-    evaluation = submission[submission["id"].isin(evaluation_rows)]
-
-    validation = submission[["id"]].merge(predictions, on="id")
-    final = pd.concat([validation, evaluation])
+    evals = submission[submission["id"].str.contains("evaluation")]
+    vals = submission[["id"]].merge(preds, on="id")
+    final = pd.concat([vals, evals])
     final.to_csv("submission.csv", index=False)
 
 
-# define list of features
-features = [
-    "item_id",
-    "dept_id",
-    "cat_id",
-    "store_id",
-    "state_id",
-    "year",
-    "month",
-    "week",
-    "day",
-    "dayofweek",
-    "event_name_1",
-    "event_type_1",
-    "event_name_2",
-    "event_type_2",
-    "snap_CA",
-    "snap_TX",
-    "snap_WI",
-    "sell_price",
-    "lag_t28",
-    "lag_t29",
-    "lag_t30",
-    "rolling_mean_t7",
-    "rolling_std_t7",
-    "rolling_mean_t30",
-    "rolling_mean_t90",
-    "rolling_mean_t180",
-    "rolling_std_t30",
-    "price_change_t1",
-    "price_change_t365",
-    "rolling_price_std_t7",
-    "rolling_price_std_t30",
-    "rolling_skew_t30",
-    "rolling_kurt_t30",
-]
+# %% [code]
+data = transform(data)
+data = feature_engineering(data)
+
+# reduce memory for new features so we can train.
+data = reduce_mem_usage(data)
+model, test = run_lgb(data)
+
+# %% [code]
+from mlflow_extend import plotting as mplt
 
 
-def transform_train_and_eval(data):
-    data = transform(data)
-    data = simple_fe(data)
-    # reduce memory for new features so we can train
-    data = reduce_mem_usage(data)
-    test = run_lgb(data)
-    predict(test, submission)
+imp_type = "gain"
+features = model.feature_name()
+importances = model.feature_importance(imp_type)
+mplt.feature_importance(features, importances, imp_type, limit=30)
 
-
-transform_train_and_eval(data)
+# %% [code]
+make_submission(test, submission)
