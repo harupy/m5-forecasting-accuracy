38dbbe8fcfa5d24b214a999fea3c4575b318d1fa
harupy <hkawamura0130@gmail.com>
2020-03-14 18:54:05 +0900

    Update plot format

--- src/main.py
+++ src/main.py
@@ -337,16 +337,16 @@ def plot_cv_indices(cv, X, y, dt_col, lw=10):
         )
 
     # Formatting
+    fontsize = 15
     yticklabels = list(range(n_splits))
-    ax.set(
-        yticks=np.arange(n_splits) + 0.5,
-        yticklabels=yticklabels,
-        xlabel="Datetime",
-        ylabel="CV iteration",
-        xlim=[X[dt_col].min(), X[dt_col].max()],
-    )
+    ax.set_xlabel("Datetime", fontsize=fontsize)
+    ax.set_xlim([X[dt_col].min(), X[dt_col].max()])
+    ax.set_ylabel("CV iteration", fontsize=fontsize)
+    ax.set_yticks(np.arange(n_splits) + 0.5)
+    ax.set_yticklabels(yticklabels)
     ax.invert_yaxis()
-    ax.set_title("{}".format(type(cv).__name__), fontsize=15)
+    ax.tick_params(axis="both", which="major", labelsize=fontsize)
+    ax.set_title("{}".format(type(cv).__name__), fontsize=20)
     return ax
 
 
