35a7fa068ee02193d0c5e3a7ef8b92cb53a4af6e
harupy <hkawamura0130@gmail.com>
2020-03-15 23:57:34 +0900

    Use h1

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # %% [markdown]
-# ## This kernel is:
+# # This kernel is:
 # ## - Based on [Very fst Model](https://www.kaggle.com/ragnar123/very-fst-model). Thanks [@ragnar123](https://www.kaggle.com/ragnar123).
 # ## - Automatically uploaded by [push-kaggle-kernel](https://github.com/harupy/push-kaggle-kernel).
 # ## - Formatted by [Black](https://github.com/psf/black).
