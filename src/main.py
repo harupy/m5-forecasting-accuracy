3e47ca22e80be4a0db9acdd35adb5fd3f865ea8d
harupy <hkawamura0130@gmail.com>
2020-03-17 10:48:57 +0900

    Fix n_splits

--- src/main.py
+++ src/main.py
@@ -424,7 +424,7 @@ class CustomTimeSeriesSplitter:
 
 # %% [code]
 cv_params = {
-    "n_splits": 1,
+    "n_splits": 2,
     "train_days": 365 * 2,
     "test_days": DAYS_PRED,
     "dt_col": dt_col,
