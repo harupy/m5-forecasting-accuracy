108aa7db1f5ffc0500dca7b294cb91c73a684ca9
harupy <hkawamura0130@gmail.com>
2020-03-23 20:32:00 +0900

    Refactor reduce_mem_usage by using to_numeric

--- src/baseline.py
+++ src/baseline.py
@@ -52,35 +52,16 @@ if on_kaggle():
 
 # %% [code]
 def reduce_mem_usage(df, verbose=False):
-    numerics = ["int16", "int32", "int64", "float16", "float32", "float64"]
     start_mem = df.memory_usage().sum() / 1024 ** 2
-    for col in df.columns:
-        col_type = df[col].dtypes
-        if col_type in numerics:
-            c_min = df[col].min()
-            c_max = df[col].max()
-            if str(col_type)[:3] == "int":
-                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
-                    df[col] = df[col].astype(np.int8)
-                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
-                    df[col] = df[col].astype(np.int16)
-                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
-                    df[col] = df[col].astype(np.int32)
-                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)
-            else:
-                if (
-                    c_min > np.finfo(np.float16).min
-                    and c_max < np.finfo(np.float16).max
-                ):
-                    df[col] = df[col].astype(np.float16)
-                elif (
-                    c_min > np.finfo(np.float32).min
-                    and c_max < np.finfo(np.float32).max
-                ):
-                    df[col] = df[col].astype(np.float32)
-                else:
-                    df[col] = df[col].astype(np.float64)
+    int_columns = df.select_dtypes(include=["int16", "int32", "int64"]).columns
+    float_columns = df.select_dtypes(include=["float16", "float32", "float64"]).columns
+
+    for col in int_columns:
+        df[col] = pd.to_numeric(df[col], downcast="integer")
+
+    for col in float_columns:
+        df[col] = pd.to_numeric(df[col], downcast="float")
+
     end_mem = df.memory_usage().sum() / 1024 ** 2
     if verbose:
         print(
@@ -94,7 +75,8 @@ def reduce_mem_usage(df, verbose=False):
 # %% [code]
 def read_data():
     INPUT_DIR = "/kaggle/input" if on_kaggle() else "input"
-    INPUT_DIR = f"{INPUT_DIR}/m5-forecasting-accuracy"
+    # INPUT_DIR = f"{INPUT_DIR}/m5-forecasting-accuracy"
+    INPUT_DIR = f"{INPUT_DIR}/sample"
 
     print("Reading files...")
 
