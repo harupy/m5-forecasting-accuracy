274fe637c4e7dc3ebf59a7ebd8d63328d286e8c6
harupy <hkawamura0130@gmail.com>
2020-04-12 12:03:03 +0900

    Update convert script

--- tools/convert.sh
+++ tools/convert.sh
@@ -2,7 +2,9 @@
 
 for file in $(find src -type f -name '*.py')
 do
+  # Convert scripts containing notebook syntax.
   if grep -q "# %%" "$file"; then
-    jupytext --to notebook $file
+    out=$(echo $file | sed -e "s/\.py/\.ipynb/g" | sed -e "s/src/src\/notebooks/g")
+    jupytext --to notebook $file -o $out
   fi
 done
